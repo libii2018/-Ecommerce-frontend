f737d48eaa3866ef3428d4e01fa3461f972a3a3f
LIBII <libiijoseph@gmail.com>
2025-09-10 19:16:06 +0100

    Organizing Files, Putting All Together and Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Products from './components/Products'
+import Products from './components/products/Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Products />
+      <Products  />
     </div>
   )
 }

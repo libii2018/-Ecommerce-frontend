9fd3868933de41ece0ecc9ba661ca9c4613841f7
LIBII <libiijoseph@gmail.com>
2025-09-11 11:48:59 +0100

    Building the About Page

--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -11,7 +11,8 @@ const ProductCard = ({
     quantity,
     price,
     discount,
-    specialPrice
+    specialPrice,
+    about = false
 }) => {
     const [openProductViewModal, setOpenProductViewModal] = useState(false);
     const btnLoader = false;
@@ -19,8 +20,10 @@ const ProductCard = ({
     const isAvailable = quantity && Number(quantity) > 0;
 
     const handleProductView = (product) => {
-        setSelectedViewProduct(product);
-        setOpenProductViewModal(true);
+        if(!about) {
+            setSelectedViewProduct(product);
+            setOpenProductViewModal(true);
+        }
     }
     return (
         <div className="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
@@ -63,34 +66,36 @@ const ProductCard = ({
                         {truncateText(description, 80)}
                     </p>
                 </div>
-                <div className="flex items-center justify-between">
-                    {specialPrice ? (
-                        <div className="flex flex-col">
-                            <span className="text-gray-400 line-through">
+                {!about && (
+                    <div className="flex items-center justify-between">
+                        {specialPrice ? (
+                            <div className="flex flex-col">
+                                <span className="text-gray-400 line-through">
+                                    ${Number(price).toFixed(2)}
+                                </span>
+                                <span className="text-xl font-bold text-slate-700">
+                                    ${Number(specialPrice).toFixed(2)}
+                                </span>
+                            </div>
+                        ) : (
+                        <span className="text-xl font-bold text-slate-700">
+                                {" "}
                                 ${Number(price).toFixed(2)}
                             </span>
-                            <span className="text-xl font-bold text-slate-700">
-                                ${Number(specialPrice).toFixed(2)}
-                            </span>
-                        </div>
-                    ) : (
-                    <span className="text-xl font-bold text-slate-700">
-                            {" "}
-                            ${Number(price).toFixed(2)}
-                        </span>
-                    )}
+                        )}
 
-                    <button 
-                        disabled={!isAvailable || btnLoader}
-                        onClick={() => {
+                        <button 
+                            disabled={!isAvailable || btnLoader}
+                            onClick={() => {
 
-                        }}
-                        className={`bg-blue-500 ${isAvailable} ? "opacity-100 hover:bg-blue-600 " : "opacity-70 "
-                                    text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
-                        <FaShoppingCart />
-                        {isAvailable ? "Add to Cart" : "Stock Out"}
-                    </button>
-                </div>
+                            }}
+                            className={`bg-blue-500 ${isAvailable} ? "opacity-100 hover:bg-blue-600 " : "opacity-70 "
+                                        text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
+                            <FaShoppingCart />
+                            {isAvailable ? "Add to Cart" : "Stock Out"}
+                        </button>
+                    </div>
+                )}
             </div>
             <ProductViewModal
                 open={openProductViewModal}

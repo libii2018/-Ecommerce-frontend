7deb63a83edcab6f87285ead7a7e2b7bb7c660a5
LIBII <libiijoseph@gmail.com>
2025-09-11 09:20:07 +0100

    Structuring Thoughts,  Setting up Routing, Setting up Banner on Home Page and Displaying Products on Home Page

---
+++ src/utils/constant.js
@@ -0,0 +1,9 @@
+import bannerImageOne from "../assets/sliders/s_1.jpg";
+import bannerImageTwo from "../assets/sliders/s_2.jpg";
+import bannerImageThree from "../assets/sliders/s_3.jpg";
+
+export {
+    bannerImageOne,
+    bannerImageTwo,
+    bannerImageThree,
+}
